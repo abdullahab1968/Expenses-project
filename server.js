857a00363111f25d6a9aef111dbb9159f0b44116
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-14 16:25:36 +0200

    create a /expense post route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const api = require('./server/routes/api')
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// app.use(express.static(path.join(__dirname, 'node_modules')))
+
 const mongoose = require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/expensesDB", {
   useNewUrlParser: true,
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,11 +2,29 @@ const express = require('express')
 const router = express.Router()
 
 const Expense = require('../models/Expense')
+const moment = require('moment');
 
 router.get('/expenses', function (req, res) {
    Expense.find({}).sort({date: -1}).then(function(expenses){
     res.send(expenses)
    })
+})
+
+router.post('/expense', function(req, res){
+      const item = req.query.item
+      const amount = req.query.amount
+      const group = req.query.group
+      const date = req.query.date ? moment(req.query.date, "YYYY-MM-DD").format('LLLL') : moment().format('LLLL')
+      const expense = new Expense({
+         amount: amount,
+         group: group,
+         date: date,
+         item: item
+      })
+      expense.save().then(function(){
+         console.log(`The amount of the expense is ${amount} is spent on ${item}`)
+         res.end()
+      })
 })
 
 module.exports = router
