bf2e93de29076cea9803ddd71e3b76aeba2029bf
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-14 15:01:41 +0200

    respond with all the expenses in sorting order by date

--- server/models/Expense.js
+++ server/models/Expense.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/peopleDB", {
-  useNewUrlParser: true,
-}).catch((err)=> console.log(err))
 const Schema = mongoose.Schema
 
 const expenseSchema = new Schema({
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const Expense = require('../models/Expense')
 
 router.get('/expenses', function (req, res) {
-   
+   Expense.find({}).sort({date: -1}).then(function(expenses){
+    res.send(expenses)
+   })
 })
 
 module.exports = router
