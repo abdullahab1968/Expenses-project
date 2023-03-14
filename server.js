75ef87f8e98b9d2e592f5152f77288b729ea7302
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-14 13:44:08 +0200

    Project structure intializations

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const api = require('./server/routes/api')
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/expensesDB", {
+  useNewUrlParser: true,
+}).catch((err)=> console.log(err))
+
+app.use('/', api)
+
+const port = 3000
+app.listen(port, function () {
+    console.log(`Running on port ${port}`)
+})
---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const Person = require('../models/Expense')
+
+router.get('/expenses', function (req, res) {
+   
+})
+
+module.exports = router
